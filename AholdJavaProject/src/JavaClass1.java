4633da83ed0bee643c9c096622d1d6c9a12521bd
Hari G <Hari.Gurunathan@cognizant.com>
2016-09-29 11:02:28 +0530

    IssueID::UST18 OwnerId::1005008184 entityType::AUSRST Status::Done Reviewer::admin Comments::commited for JIRA use case

--- AholdJavaProject/src/JavaClass1.java
+++ AholdJavaProject/src/JavaClass1.java
@@ -4,6 +4,7 @@ public class JavaClass1 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World against UST17 ToDo");
+		System.out.println("Hello World against UST18 ToDo");
 	}
 
 }
