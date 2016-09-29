431eafe671055e13d2434bb3d6ba1382ef6b3aed
Hari G <Hari.Gurunathan@cognizant.com>
2016-09-29 13:58:02 +0530

    IssueID::UST22 OwnerId::1005008184 entityType::AUSRST Status::Done Reviewer::admin Comments::Test

--- AholdJavaProject/src/JavaClass1.java
+++ AholdJavaProject/src/JavaClass1.java
@@ -4,6 +4,7 @@ public class JavaClass1 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World against UST21");
+		System.out.println("Hello World against UST22");
 	}
 
 }
